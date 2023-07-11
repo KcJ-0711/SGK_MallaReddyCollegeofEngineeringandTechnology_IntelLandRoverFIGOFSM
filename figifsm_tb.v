c2e8cff5eb0bbf23d5ce872fb0c013d769c78d9f
KcJ-0711 <117647140+KcJ-0711@users.noreply.github.com>
2023-07-11 22:00:27 +0530

    Updated the code to fix known errors
    Changes have been made to testbench delay to give out the current state.

--- figifsm_tb.v
+++ figifsm_tb.v
@@ -25,17 +25,45 @@ end
 
 initial begin
 
-ins = 5'b10101;
+ins = 5'b00001;
 
 $monitor ("time=%d, in=%b, out=%b", $time, in, out);
 
 for(i = 4; i > 0; i = i - 1) begin
 
 in = ins[i];
-#5;
+#10;
+
+end
+
+
+#30;
+reset = 1'b1;
+#1;
+reset = 1'b0;
+
+for(i = 4; i > 0; i = i - 1) begin
+
+in = ins[i];
+#10;
+
+end
+
+#60;
+reset = 1'b1;
+#1;
+reset = 1'b0;
+
+ins = ins + 5'b00001;
+
+for(i = 4; i > 0; i = i - 1) begin
+
+in = ins[i];
+#10;
 
 end
 
 end
+
 
 endmodule
